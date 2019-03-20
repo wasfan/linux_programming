2c27b5253b1cc2e93991f2d90b19f48b34aa40f7
Frank Wang <wasfan@yahoo.com>
2019-03-20 15:31:12 +0800

    add Linux_Raider
    add Linux_Raider

---
+++ Linux_Raider/ch03_mem/testConst.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+char *a="hello";
+int main(void)
+{
+ 	char *b = "hello";
+ 	printf("in main:");
+ 	if(a==b)
+  		  printf("yes,a==b\n");
+ 	else
+  		  printf("no,a!=b\n");
+ 	
+   	printf("in sub:");
+ 	test();
+ 	return 0;
+}
+
